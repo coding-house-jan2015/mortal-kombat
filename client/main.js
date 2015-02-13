323fb35eb34e9abdb858b710e00feebba3d79013
Chyld Medford <chyld.medford@gmail.com>
2015-02-12 16:12:38 -0800

    gulp media

--- client/main.js
+++ client/main.js
@@ -1,1 +1,12 @@
-console.log('hello, world');
+'use strict';
+
+$(document).ready(init);
+
+function init() {
+  playTheme();
+}
+
+function playTheme() {
+  $('audio').attr('src', '/audio/theme.mp3');
+  $('audio')[0].play()
+}
